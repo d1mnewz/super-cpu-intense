547ae8525a3df3462dc7202adbb31fbdaf9a8bf0
Dimko Zhluktenko <d1mnewz@gmail.com>
2021-12-08 16:34:10 +0200

    ...

--- cpu-worker/Program.cs
+++ cpu-worker/Program.cs
@@ -2,15 +2,18 @@
 
 using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 
 Console.WriteLine("Hello, Portainer!");
-
+Console.WriteLine("Will I crash?");
 const int percentage = 80;
+
 for (var i = 0; i < Environment.ProcessorCount; i++)
 {
     new Thread(() =>
     {
+        var s = "s";
         Stopwatch watch = new();
         watch.Start();
         while (true)
@@ -18,6 +21,8 @@ for (var i = 0; i < Environment.ProcessorCount; i++)
             // remaining percentage milliseconds. So 40% utilization means work 40ms and sleep 60ms
             if (watch.ElapsedMilliseconds > percentage)
             {
+                Console.WriteLine("It's high CPU & RAM if you can read it... Save your laptop friend");
+                s += string.Join('.', Enumerable.Range(0,100).Select(x => x.ToString()));
                 Thread.Sleep(100 - percentage);
                 watch.Reset();
                 watch.Start();
